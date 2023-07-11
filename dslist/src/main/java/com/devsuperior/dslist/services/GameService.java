1a9ff66d1568b697d5c84decb1a3fe9d4ccd6e84
cainamicael <cainamicaeloficial@gmail.com>
2023-07-10 23:30:01 -0300

    add

--- dslist/src/main/java/com/devsuperior/dslist/services/GameService.java
+++ dslist/src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
 
@@ -14,10 +15,13 @@ public class GameService {
 	@Autowired //Para injetar uma instancia do game repository
 	private GameRepository gameRepository;
 	
-	public List<Game> findAll() {
+	public List<GameMinDTO> findAll() {
 		//Pegamos todos os games
 		List<Game> result = gameRepository.findAll(); //Recebe tudo do bd
-		return result;
+		
+		//Tratando nosso resultado, resumindo as informações para nosso dto
+		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+		return dto;
 	}
 	
 }
